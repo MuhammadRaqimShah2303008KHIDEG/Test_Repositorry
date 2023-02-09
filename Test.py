d6c33149a7632b0a14ee93e76bb25884d7b8a7e5
MuhammadRaqimShah2303008KHIDEG <124855765+MuhammadRaqimShah2303008KHIDEG@users.noreply.github.com>
2023-02-09 15:44:16 +0500

    updated

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
-print("a")
+from flask import Flask
+
+app= Flask(__name__)
+@app.route('/')
+
+def test():
+    return  "Test repository to learn GitHub"
+
+@app.route('/another_route')
+
+def another_route():
+    return  "Test repository to learn GitHub USING ANOTHER ROUTING TECHNIQUE "
+
+if __name__=="__main__":
+    app.run(debug=True)
